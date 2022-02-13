09ce3e44e2e31faa94c0a161d0bb6a36c869fc80
rakibul-wdp <rakibul.wdp@gmail.com>
2022-02-13 17:09:51 +0600

    28-7 Verify match and use function to reduce duplicate

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ document.getElementById("key-pad").addEventListener("click", function (event) {
   const calcInput = document.getElementById("typed-numbers");
   if (isNaN(number)) {
     if (number == "C") {
-      calcInput.value = '';
+      calcInput.value = "";
     }
   } else {
     const previousNumber = calcInput.value;
@@ -25,3 +25,17 @@ document.getElementById("key-pad").addEventListener("click", function (event) {
     calcInput.value = newNumber;
   }
 });
+
+function verifyPin() {
+  const pin = document.getElementById("display-pin").value;
+  const typedNumbers = document.getElementById("typed-numbers").value;
+  const successMessage = document.getElementById("notify-success");
+  const failError = document.getElementById("notify-fail");
+  if (pin == typedNumbers) {
+    successMessage.style.display = "block";
+    failError.style.display = "none";
+  } else {
+    failError.style.display = "block";
+    successMessage.style.display = "none";
+  }
+}
