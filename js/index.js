682533cb6cda7ab18beef17235ea261917044216
rakibul-wdp <rakibul.wdp@gmail.com>
2022-02-13 19:22:14 +0600

    set less than key work

--- js/index.js
+++ js/index.js
@@ -18,6 +18,8 @@ document.getElementById("key-pad").addEventListener("click", function (event) {
   if (isNaN(number)) {
     if (number == "C") {
       calcInput.value = "";
+    } else {
+      calcInput.value = calcInput.value.slice(0, -1);
     }
   } else {
     const previousNumber = calcInput.value;
